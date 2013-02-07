a9a23b3e4eb11283d2b24eddce0ce5eadc9a6c52
unknown <pereter92@gmail.com>
2013-02-07 14:58:25 +0200

    terrain scene added

---
+++ 1/Assets/My Scripts/MousePick.js
@@ -0,0 +1,6 @@
+private var picknum = 0;
+
+function  OnMouseDown () {
+picknum = picknum + 1;
+print("Object was picked " + picknum);
+}
--- 1/Assets/My Scripts/SimpleMove.js
+++ 1/Assets/My Scripts/SimpleMove.js
@@ -1,4 +1,7 @@
-
+var mySpeed : float;
+var someString : String = "This is a test";
+var someSettings : boolean = true;
+var someObject : GameObject;
 var Speed = 0.1;
 
 function Update () {
